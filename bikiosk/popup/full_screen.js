964b83bbce1c1cc720bdd2acd7bcf0b3b9d80bcd
Viter Dar <viter@itband.org>
2020-06-17 00:05:45 +0300

    1.1.0 The first release: full-screen and reload

---
+++ bikiosk/popup/full_screen.js
@@ -0,0 +1,29 @@
+
+//console.log("biKiosk : popup/full_screen.js is loading...");
+
+/**
+ * send a "full_screen" message to the background script.
+ */
+function full_screen()
+{
+  //console.log("biKiosk : send event : full_screen");
+  browser.runtime.sendMessage({ command: "full-screen" });
+}
+
+
+/**
+ * send a "ff_reload" message to the background script.
+ */
+function ff_reload()
+{
+  //console.log("biKiosk : send event : ff_reload");
+  browser.runtime.sendMessage({ command: "ff-reload" });
+}
+
+
+document.querySelector("#full-screen").addEventListener('click', full_screen );
+document.querySelector("#ff-reload").addEventListener('click', ff_reload );
+
+
+console.log("biKiosk : popup/full_screen.js wass loaded");
+
